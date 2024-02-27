2b2e3b64a54d6e6d2fc2f57d88c5e196fff480e3
lukha243 <lukha243@student.liu.se>
2024-02-27 13:46:26 +0100

    start på project

---
+++ src/se/liu/liuid123/Client.java
@@ -0,0 +1,50 @@
+package se.liu.liuid123;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client
+{
+    public static void main(String[] args) {
+	final ServerSocket serverSocket;
+	final Socket clientSocket;
+	final BufferedReader in;
+	final PrintWriter out;
+	final Scanner scanner = new Scanner(System.in);
+	try{
+	    serverSocket = new ServerSocket(5000);
+	    clientSocket = serverSocket.accept();
+	    out = new PrintWriter(clientSocket.getOutputStream());
+	    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+	    Thread sender = new Thread(new Runnable()
+	    {
+		String msg;
+		@Override public void run() {
+		    while (true){
+			msg = scanner.nextLine();
+			out.println(msg);
+			out.flush();
+		    }
+		}
+	    });
+	    sender.start();
+
+	    Thread recieve = new Thread(new Runnable()
+	    {
+		String msg = in.readLine();
+		@Override public void run() {
+
+		}
+	    });
+
+	}catch (IOException io){
+	    io.printStackTrace();
+	}
+    }
+}
---
+++ src/se/liu/liuid123/Server.java
@@ -0,0 +1,5 @@
+package se.liu.liuid123;
+
+public class Server
+{
+}
